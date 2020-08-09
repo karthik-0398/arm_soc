a863fcb697819ca49deceeb5dcb0ceb6289be952
ks6n19 <ks6n19@soton.ac.uk>
2020-08-10 00:04:09 +0100

    Update testbench/de0_wrapper.tcl Deleted testbench/de0_wrapper_stim.sv, testbench/nexys4_wrapper.tcl, testbench/nexys4_wrapper_stim.sv, testbench/de2_wrapper_stim.sv files

--- testbench/de0_wrapper.tcl
+++ testbench/de0_wrapper.tcl
@@ -9,14 +9,21 @@ simvision {
 
   # Add Waves
 
-    waveform  add  -signals  de0_wrapper_stim.CLOCK_50
-    waveform  add  -signals  de0_wrapper_stim.KEY
-    waveform  add  -signals  de0_wrapper_stim.SW
-    waveform  add  -signals  de0_wrapper_stim.LEDG
-    waveform  add  -signals  de0_wrapper_stim.HEX0
-    waveform  add  -signals  de0_wrapper_stim.HEX1
-    waveform  add  -signals  de0_wrapper_stim.HEX2
-    waveform  add  -signals  de0_wrapper_stim.HEX3
+    waveform  add  -signals  de1_soc_wrapper_stim.CLOCK_50
+    waveform  add  -signals  de1_soc_wrapper_stim.SW
+    waveform  add  -signals  de1_soc_wrapper_stim.LEDR
+    waveform  add  -signals  de1_soc_wrapper_stim.HEX0
+    waveform  add  -signals  de1_soc_wrapper_stim.HEX1
+    waveform  add  -signals  de1_soc_wrapper_stim.HEX2
+    waveform  add  -signals  de1_soc_wrapper_stim.HEX3
+    waveform  add  -signals  de1_soc_wrapper_stim.KEY
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_R
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_G
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_B
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_HS
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_VS
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_CLK
+    waveform  add  -signals  de1_soc_wrapper_stim.VGA_BLANK_N
     waveform  add  -signals  de0_wrapper_stim.dut.soc_inst.HADDR
     waveform  add  -signals  de0_wrapper_stim.dut.soc_inst.HWRITE
     waveform  add  -signals  de0_wrapper_stim.dut.soc_inst.HSEL_RAM
